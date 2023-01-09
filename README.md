55f6f6da070b2bbac3b6833ebdf74332f73e3e8d
vanloc1808 <vanloc1808@gmail.com>
2023-01-09 11:48:02 +0700

    init server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Dating-App-Server
+https://www.youtube.com/watch?v=kshZa-71kW0&list=PLZEIt444jqpB1j3RD4BrYm9JmNVYuzVNm&index=2
+https://www.youtube.com/watch?v=jBzwzrDvZ18
---
+++ run.sh
@@ -0,0 +1,11 @@
+# python3 -m venv ./venv
+source venv/bin/activate
+
+# python3 -m pip install --upgrade pip
+# pip install django
+# pip install djangorestframework
+# django-admin startproject proj .
+# django-admin startapp dating_app
+# python3 manage.py migrate
+
+python3 manage.py runserver
